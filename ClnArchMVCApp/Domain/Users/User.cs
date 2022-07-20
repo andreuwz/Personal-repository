f87a0c71e294d9df1c464a90dab9edbce8273146
Andreuwz <end17y1@gmail.com>
2022-07-20 12:58:05 +0300

    Initial commit

---
+++ ClnArchMVCApp/Domain/Users/User.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+
+namespace Domain.Users
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public string Firstname { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
