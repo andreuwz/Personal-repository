27df30639aa0cd678cd5a1ace5eb29fd05ad8360
Andreuwz <end17y1@gmail.com>
2022-07-21 16:35:29 +0300

    Added design time db context so visual studio scaffolding can work. Also removed duplicated repository.

---
+++ ClnArchMVCApp/Persistance/DbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Persistance
+{
+    public class DbContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+    {
+        public DatabaseContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+            optionsBuilder.UseSqlServer("Server=.;Database=FurnitureShopDB;Trusted_Connection=True;");
+
+            return new DatabaseContext(optionsBuilder.Options);
+        }
+    }
+}
