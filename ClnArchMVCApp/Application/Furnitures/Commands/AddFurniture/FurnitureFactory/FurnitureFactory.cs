407a60abf6842a47afd43fd04079aacf280d58ae
Andreuwz <end17y1@gmail.com>
2022-07-21 16:32:44 +0300

    Added commands for creation of furniture.

---
+++ ClnArchMVCApp/Application/Furnitures/Commands/AddFurniture/FurnitureFactory/FurnitureFactory.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces.Persistence;
+using Domain.Furnitures;
+
+namespace Application.Furnitures.Commands.AddFurniture.FurnitureFactory
+{
+    public class FurnitureFactory : IFurnitureFactory
+    {
+        private readonly IFurnitureRepository furnitureRepository;
+
+        public FurnitureFactory(IFurnitureRepository furnitureRepository)
+        {
+            this.furnitureRepository = furnitureRepository;
+        }
+
+        public Furniture Execute(string name, string type, string description)
+        {
+
+            Furniture newItem = new Furniture()
+            {
+                Name = name,
+                Type = type,    
+                Description = description
+            };
+            furnitureRepository.Add(newItem);
+            return newItem;
+        }
+    }
+}
---
+++ ClnArchMVCApp/Application/Furnitures/Commands/AddFurniture/FurnitureFactory/IFurnitureFactory.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Furnitures;
+
+namespace Application.Furnitures.Commands.AddFurniture.FurnitureFactory
+{
+    public interface IFurnitureFactory
+    {
+        Furniture Execute(string name, string type, string description);
+    }
+}
