d86b9dcee205453d2db811659bf1980067fb0764
Andreuwz <end17y1@gmail.com>
2022-07-21 16:33:07 +0300

    Added commands for updating furnitures.

---
+++ ClnArchMVCApp/Application/Furnitures/Commands/UpdateFurniture/IUpdateFurniture.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Furnitures;
+
+namespace Application.Furnitures.Commands.UpdateFurniture
+{
+    public interface IUpdateFurniture
+    {
+        void Execute(Furniture furniture);
+    }
+}
---
+++ ClnArchMVCApp/Application/Furnitures/Commands/UpdateFurniture/UpdateFurniture.cs
@@ -0,0 +1,27 @@
+﻿using Application.Interfaces.Persistence;
+using Domain.Furnitures;
+
+namespace Application.Furnitures.Commands.UpdateFurniture
+{
+    public class UpdateFurniture : IUpdateFurniture
+    {
+        private readonly IFurnitureRepository furnitureRepository;
+
+        public UpdateFurniture(IFurnitureRepository furnitureRepository)
+        {
+            this.furnitureRepository = furnitureRepository;
+        }
+
+        public void Execute(Furniture furniture)
+        {
+            var foundModel = furnitureRepository.Get(furniture.Id);
+            furnitureRepository.Update(new Furniture()
+            {
+                Description = foundModel.Description,
+                Type = foundModel.Type,
+                Name = foundModel.Name
+            });
+            
+        }
+    }
+}
