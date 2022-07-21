4c66c9b0a9c932ef3fe3ed7712f89a37e7fd8be3
Andreuwz <end17y1@gmail.com>
2022-07-21 11:52:32 +0300

    Further implementing the repository pattern

---
+++ ClnArchMVCApp/Persistance/Repositories/Users/UserRepository.cs
@@ -0,0 +1,8 @@
+﻿using Application.Interfaces.Persistence;
+
+namespace Persistance.Repositories.Users
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+    }
+}
---
+++ ClnArchMVCApp/Persistance/Repository.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces.Persistence;
+using Domain.Common;
+
+namespace Persistance
+{
+    public class Repository<T> : IRepository<T> where T : class, IEntity
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
