dc11e4be224e076c6b09417fd28c0f3fff2611d2
Andreuwz <end17y1@gmail.com>
2022-07-21 10:02:35 +0300

    Creation of get all query. Basic form.

---
+++ ClnArchMVCApp/Application/Furnitures/Queries/GetAllFurnituresList/GetAllFurnituresListQuery.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces.Persistence;
+using Domain.Furnitures;
+
+namespace Application.Furnitures.Queries.GetAllFurnituresList
+{
+    public class GetAllFurnituresQuery : IGetAllFurnituresListQuery
+    {
+
+        private readonly IUserRepository userRepository;
+
+        public GetAllFurnituresQuery(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public IEnumerable<Furniture> GetAllFurnitures()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClnArchMVCApp/Application/Furnitures/Queries/GetAllFurnituresList/IGetAllFurnituresListQuery.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Furnitures;
+
+namespace Application.Furnitures.Queries.GetAllFurnituresList
+{
+    public interface IGetAllFurnituresListQuery
+    {
+        public IEnumerable<Furniture> GetAllFurnitures();
+    }
+}
