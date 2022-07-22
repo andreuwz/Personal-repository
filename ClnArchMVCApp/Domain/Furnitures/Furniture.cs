4a6ccf58aa230be20f5eaa203a4b9d907ca05fdd
Andreuwz <end17y1@gmail.com>
2022-07-22 09:46:35 +0300

    Placed required attributes.

--- ClnArchMVCApp/Domain/Furnitures/Furniture.cs
+++ ClnArchMVCApp/Domain/Furnitures/Furniture.cs
@@ -1,13 +1,17 @@
 ﻿using Domain.Common;
 using Domain.Users;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Furnitures
 {
     public class Furniture : IEntity
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Type { get; set; }
+        [Required]
         public string Description { get; set; }
 
         public List<User> Users { get; set; }
