a275c4eb876bdca7a5866999acd9bfa4ffd1e249
Andreuwz <end17y1@gmail.com>
2022-07-25 10:53:14 +0300

    Introduciton of User DTO, removal of Id, Password fields from views.

---
+++ ClnArchMVCApp/Application/Users/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Users
+{
+    public class UserModel
+    {
+        [Required]
+        [MaxLength(10)]
+        public string Username { get; set; }
+       
+        [Required]
+        public bool IsAdmin { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Firstname { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
