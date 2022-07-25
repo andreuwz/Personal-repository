ce7eadffe7aa47456c2f441d15cd347aa6c6d644
Andreuwz <end17y1@gmail.com>
2022-07-25 09:31:39 +0300

    Added business logic for buying furniture item.

---
+++ ClnArchMVCApp/Application/Furnitures/Commands/BuyFurniture/BuyFurniture.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interfaces.Persistence;
+using Domain.Furnitures;
+
+namespace Application.Furnitures.Commands.BuyFurniture
+{
+    public class BuyFurniture : IBuyFurniture
+    {
+        private readonly IFurnitureRepository furnitureRepository;
+
+        public BuyFurniture(IFurnitureRepository furnitureRepository)
+        {
+            this.furnitureRepository = furnitureRepository;
+        }
+
+        public Furniture Execute(int id, int quantity)
+        {
+            var foundModel = furnitureRepository.Get(id);
+
+            if (foundModel.Quantity >= quantity)
+            {
+                foundModel.Quantity -= quantity;
+                furnitureRepository.Update(foundModel);
+
+                return foundModel;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ ClnArchMVCApp/Application/Furnitures/Commands/BuyFurniture/IBuyFurniture.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Furnitures;
+
+namespace Application.Furnitures.Commands.BuyFurniture
+{
+    public interface IBuyFurniture
+    {
+        Furniture Execute(int id, int quantity);
+    }
+}
