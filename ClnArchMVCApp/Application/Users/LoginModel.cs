ddced2b8176f3c81b783b8c1727575e288ad858d
Andreuwz <end17y1@gmail.com>
2022-07-25 19:32:02 +0300

    Introduction of login model, so that it can make modelstate validations,

---
+++ ClnArchMVCApp/Application/Users/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Users
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
