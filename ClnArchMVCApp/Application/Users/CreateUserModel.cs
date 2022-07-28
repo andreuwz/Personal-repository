f2f8681915f8072f67163bcd67110a62e360796b
Andreuwz <end17y1@gmail.com>
2022-07-28 10:26:59 +0300

    Introduction of create user model.

---
+++ ClnArchMVCApp/Application/Users/CreateUserModel.cs
@@ -0,0 +1,30 @@
+﻿using Application.ModelValidation;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Application.Users
+{
+    public class CreateUserModel
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(10)]
+        [MinLength(5)]
+        public string Username { get; set; }
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", ErrorMessage ="Password is not compatible.")] //8 chars, one digit, one special character
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        public bool IsAdmin { get; set; }
+        [Required]
+        [MaxLength(15)]
+        [MinLength(5)]
+        public string Firstname { get; set; }
+        [Required]
+        [ObsoleteDateValidation(ErrorMessage ="You cannot enter past dates (check time).")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
