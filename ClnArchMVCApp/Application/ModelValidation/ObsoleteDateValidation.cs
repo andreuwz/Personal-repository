4076b89194d9045de645071f2b088c1934a29a83
Andreuwz <end17y1@gmail.com>
2022-07-28 10:23:56 +0300

    Custom modelstate validation for datetime.

---
+++ ClnArchMVCApp/Application/ModelValidation/ObsoleteDateValidation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.ModelValidation
+{
+    public class ObsoleteDateValidation : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+
+            DateTime dateTime = Convert.ToDateTime(value);
+            if (dateTime < DateTime.Now)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
